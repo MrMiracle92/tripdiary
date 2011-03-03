13f3a897bb5ebcb04b18edf1d5b8f1345efe6b62
saha.arpita <saha.arpita@a595842d-2d1c-385a-50ee-7da7d0d81d52>
2011-03-03 03:40:29 +0000

    Changes for having a pop-up window with information regarding the application. Still need to choose a better color scheme.

---
+++ trunk/TripDiary/src/com/google/code/p/tripdiary/TripDiaryInfo.java
@@ -0,0 +1,53 @@
+package com.google.code.p.tripdiary;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.ViewGroup;
+import android.widget.PopupWindow;
+
+/**
+ * Display a pop-up window with information regarding the application.
+ * @author Arpita Saha
+ *
+ */
+public class TripDiaryInfo extends Activity {
+	private PopupWindow infoPopupWindow;
+	private View layout;
+
+	/** Called when the activity is first created. */
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		LayoutInflater inflater = (LayoutInflater) TripDiaryInfo.this
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+		layout = inflater.inflate(R.layout.trip_home,
+				(ViewGroup) findViewById(R.id.trip_home_info_popup));
+		// layout.setBackgroundColor(color.darker_gray);
+
+		infoPopupWindow = new PopupWindow(layout, 400, 200, true);
+		infoPopupWindow.setTouchable(true);
+		infoPopupWindow.setOutsideTouchable(true);
+		infoPopupWindow.setBackgroundDrawable(new BitmapDrawable());
+		infoPopupWindow.setTouchInterceptor(new OnTouchListener() {
+			public boolean onTouch(View v, MotionEvent event) {
+				infoPopupWindow.dismiss();
+				finish();
+				return false;
+			}
+		});
+
+		layout.findViewById(R.id.trip_home_info_popup).post(new Runnable() {
+			public void run() {
+				infoPopupWindow.showAtLocation(layout, Gravity.CENTER, 0, 0);
+			}
+		});
+	}
+}
