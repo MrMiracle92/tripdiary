9afc75b1fae8065a0d2e2dae6156606d95a664d6
saha.arpita <saha.arpita@a595842d-2d1c-385a-50ee-7da7d0d81d52>
2011-03-06 09:40:03 +0000

    Added path in KML file.

--- trunk/TripDiary/src/com/google/code/p/tripdiary/TripExport.java
+++ trunk/TripDiary/src/com/google/code/p/tripdiary/TripExport.java
@@ -91,6 +91,7 @@ public class TripExport extends Activity {
 
 	private String toXMLString() {
 		StringBuilder sb = new StringBuilder("");
+		StringBuilder lineString = new StringBuilder("");
 
 		String xmlStart = "<?xml version=\"1.0\"?>"
 				+ "<kml xmlns=\"http://www.opengis.net/kml/2.2\">"
@@ -103,42 +104,76 @@ public class TripExport extends Activity {
 				+ tripEntryCursor.getCount());
 
 		tripEntryCursor.moveToFirst();
+		boolean isFirst = true;
 
 		while (tripEntryCursor.isAfterLast() == false) {
-			// Create the <Placemark> element
-			sb.append("<Placemark>");
-
 			TripDiaryLogger.logDebug("Exporting entry "
 					+ tripEntryCursor.getString(tripEntryCursor
 							.getColumnIndex(TripDetailCols._ID))
 					+ " Content : "
 					+ tripEntryCursor.getString(tripEntryCursor
 							.getColumnIndex(TripDetailCols.MEDIA_TYPE)));
+
+			// For mediatype none, add to lineString : to draw the path.
 			if (tripEntryCursor.getString(
 					tripEntryCursor.getColumnIndex(TripDetailCols.MEDIA_TYPE))
-					.equalsIgnoreCase("TEXT")) {
-				sb.append("<description>");
-				String noteText = tripEntryCursor.getString(tripEntryCursor
-						.getColumnIndex(TripDetailCols.NOTE));
-				sb.append(noteText);
-				sb.append("</description>");
-			} else {
-				sb.append("<description>" + " Created by tripDiary application"
-						+ "</description>");
-			}
-			sb.append("<Point>");
+					.equalsIgnoreCase("NONE")) {
 
-			String latlon = tripEntryCursor.getString(tripEntryCursor
-					.getColumnIndex(TripDetailCols.LON))
-					+ ","
-					+ tripEntryCursor.getString(tripEntryCursor
-							.getColumnIndex(TripDetailCols.LAT));
-			sb.append("<coordinates>" + latlon + "</coordinates>");
-			sb.append("</Point>");
-			sb.append("</Placemark>");
+				if (isFirst == true)
+					isFirst = false;
+				else
+					lineString.append(",");
+
+				String latlon = tripEntryCursor.getString(tripEntryCursor
+						.getColumnIndex(TripDetailCols.LON))
+						+ ","
+						+ tripEntryCursor.getString(tripEntryCursor
+								.getColumnIndex(TripDetailCols.LAT)) + ",0";
+				lineString.append(latlon);
+			} else {
+				// Create the <Placemark> element
+				sb.append("<Placemark>");
+
+				// If text, add it as a description
+				if (tripEntryCursor.getString(
+						tripEntryCursor
+								.getColumnIndex(TripDetailCols.MEDIA_TYPE))
+						.equalsIgnoreCase("TEXT")) {
+					sb.append("<description>");
+					String noteText = tripEntryCursor.getString(tripEntryCursor
+							.getColumnIndex(TripDetailCols.NOTE));
+					sb.append(noteText);
+					sb.append("</description>");
+				} else { // For other media types, add default description
+					sb.append("<description>"
+							+ " Created by tripdiary application"
+							+ "</description>");
+				}
+
+				sb.append("<Point>");
+				String latlon = tripEntryCursor.getString(tripEntryCursor
+						.getColumnIndex(TripDetailCols.LON))
+						+ ","
+						+ tripEntryCursor.getString(tripEntryCursor
+								.getColumnIndex(TripDetailCols.LAT));
+				sb.append("<coordinates>" + latlon + "</coordinates>");
+				sb.append("</Point>");
+				sb.append("</Placemark>");
+			}
 
 			tripEntryCursor.moveToNext();
 		}
+
+		// Add the LineString element now
+		sb.append("<Placemark><name>");
+		sb.append("</name>");
+		sb.append("<description>" + " Created by tripdiary application"
+				+ "</description>");
+		sb.append("<styleUrl>#yellowLineGreenPoly</styleUrl>");
+		sb.append("<LineString><extrude>1</extrude><tessellate>1</tessellate><altitudeMode>absolute</altitudeMode>");
+		sb.append("<coordinates>");
+		sb.append(lineString.toString());
+		sb.append("</coordinates></LineString></Placemark>");
 
 		String xmlEnd = "</Document></kml>";
 		sb.append(xmlEnd);
@@ -150,11 +185,13 @@ public class TripExport extends Activity {
 	private boolean genAndWriteToDisk() {
 		// Generate
 		String kmlStr = toXMLString();
-		
-		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
-		String folderNameFromPref = sp.getString("folderPref", "tripdiary"); // Default is tripdiary
+
+		SharedPreferences sp = PreferenceManager
+				.getDefaultSharedPreferences(getBaseContext());
+		String folderNameFromPref = sp.getString("folderPref", "tripdiary"); // Default
+																				// is
+																				// tripdiary
 		TripDiaryLogger.logDebug("KML dir is : " + folderNameFromPref);
-		
 
 		// Create the default file storage path
 		File kmlDir = new File(Environment.getExternalStorageDirectory()
